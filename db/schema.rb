8e6516778dabd077cf0d05724a9d2ac3c9af7e0a
toru <toru0722@gmail.com>
2020-03-17 10:12:45 -0400

    add_work_model&table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_165221) do
+ActiveRecord::Schema.define(version: 2020_03_17_135411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2020_03_16_165221) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "from"
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.string "title"
+    t.datetime "release"
+    t.string "starring"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
