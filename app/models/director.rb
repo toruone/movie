80194e695e53c08b41f1ca7295c300887f4bcd9f
toru <toru0722@gmail.com>
2020-03-18 09:53:56 -0400

    add beforesave capitalize

--- app/models/director.rb
+++ app/models/director.rb
@@ -2,4 +2,9 @@ class Director < ApplicationRecord
   validates :name, presence: true
   validates :from, presence: true
   scope :by_name, ->{ order(name: :asc) }
+
+  before_save :capitalize_name
+  def capitalize_name
+    self.name = self.name.split.map(&:capitalize).join(' ')
+  end
 end
