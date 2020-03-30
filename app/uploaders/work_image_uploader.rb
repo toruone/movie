31dd8f734e74c02e6d381bcaa8a968eb272689fe
toru <toru0722@gmail.com>
2020-03-30 13:37:22 -0400

    fix

--- app/uploaders/work_image_uploader.rb
+++ app/uploaders/work_image_uploader.rb
@@ -4,11 +4,8 @@ class WorkImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    include Cloudinary::CarrierWave
-  else
-    storage :file
-  end
+  storage :file
+
   # storage :fog
 
 
