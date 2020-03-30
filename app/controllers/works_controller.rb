2c70e56fc34a6a8e5de4c17f55e9c822ae316602
toru <toru0722@gmail.com>
2020-03-30 09:09:01 -0400

    fix_directors_index_show

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,7 +2,7 @@ class WorksController < ApplicationController
   before_action :set_work, only: [:show]
 
   def index
-    @works = Work.includes(:director).by_position.page(params[:page]).per(3)
+    @works = Work.includes(:director).by_position.page(params[:page]).per(6)
   end
 
   # def create
